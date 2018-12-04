b79502bbfbd9f980662858d1cb7bb57e7d5929e9
TylerD07 <tyler.daughtry@aol.com>
2018-12-04 13:09:56 -0500

    created trackList component, added test content for posts

--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 import "./HomePage.scss";
+import TrackList from "../Tracklist/TrackList";
 
 const HomePage = props => (
   <div className="homepage-content">
     HomePage
+    <TrackList/>
   </div>
 );
 

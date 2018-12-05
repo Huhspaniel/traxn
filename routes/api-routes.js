ffd57d3dc98b775d4c32d28784742819f7778bf6
Ki <kikim1025@gmail.com>
2018-12-05 11:53:50 -0500

    Added JWT authentification middleware

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,12 +2,28 @@ const { User, Track } = require('../models');
 const bcrypt = require('bcryptjs');
 const jwt = require(`jsonwebtoken`);
 
-// Middleware for authenticating json web token
-function authenticateJWT(req, res, next) {
-    next();
-}
-
 module.exports = function (app) {
+
+    const authJWT = function (req, res, next) {
+        try {
+            const token = req.headers[`x-access-token`];
+            if (token) {
+                jwt.verify(token, app.get(`JWTKey`), function(err, decoded) { 
+                    if (err) {
+                        throw err.message; 
+                    } else {
+                        req.body.username = decoded.username; // not really necessary unless next function use it
+                        next();
+                    }
+                });
+            } else {
+                throw `No token provided`;
+            }
+        } catch(err) {
+            res.json({status: `error`, message: err});
+        }
+    }
+
     app.get(`/api/users`, (req, res) => {
         User.find({})
             .populate('following')
@@ -20,8 +36,9 @@ module.exports = function (app) {
             .then(data => res.json(data))
             .catch(err => res.json(err));
     })
+    
     app.route('/api/tracks')
-        .post(authenticateJWT, (req, res) => {
+        .post(authJWT, (req, res) => {
             Track.create(req.body)
                 .then(data => res.json(data))
                 .catch(err => res.json(err));
@@ -44,52 +61,67 @@ module.exports = function (app) {
     // Request will contest username and password
     // After comparing with database using bcrypt decryption, response with jwt token
     // If error, response with error object
-    app.post(`/api/login`, (req, res) => {
+    app.post(`/api/login`, function (req, res) {
         User.findOne({username: req.body.username})
         .then(function(data) {
             if (!data) {
-                throw "No such user or bad request format"
+                throw `No such user or bad request format`
             } else {
                 bcrypt.compare(req.body.password, data.password)
                 .then(function(bcryptRes) {
                     if (bcryptRes) {
-                        const token = jwt.sign({username: data.username}, app.get("JWTKey"), { expiresIn: "1h" });
-                        res.json({status: "success", message: "Logged in", data: {username: data.username, token: token}});   
+                        const token = jwt.sign({username: data.username}, app.get(`JWTKey`), { expiresIn: `1h` });
+                        res.json({status: `success`, message: `Logged in`, data: {username: data.username, token: token}});   
                     } else {
-                        throw "Wrong password";
+                        throw `Wrong password`;
                     }
                 })
                 .catch(function(err) {
-                    res.json({status: "error", message: err});
+                    res.json({status: `error`, message: err});
                 });
             }              
         })
         .catch(function(err) {
-            res.json({status: "error", message: err});
+            res.json({status: `error`, message: err});
         });
     });
 
+
+// Alternate method of JWT authentification, using routes rather than middleware function
+/*    
     // Authentification route
     // All routes starting with below route will be authentificated
-    // Decoded id passed to next function call
-    app.use("/api/auth", function(req, res, next) {
-        const token = req.headers["x-access-token"];
+    // Checks if the JWT token is includeded in the header
+    // Decoded username passed to next function call
+    app.use(`/api/auth`, function (req, res, next) {
+        const token = req.headers[`x-access-token`];
         try {
             if (token) {
-                jwt.verify(token, app.get('JWTKey'), function(err, decoded) { 
+                jwt.verify(token, app.get(`JWTKey`), function(err, decoded) { 
                     if (err) {
                         throw err.message;
                     } else {
-                        req.body.username = decoded.username;
+                        req.body.username = decoded.username; // not really necessary unless next function use it
                         next();
                     }
                 });
             } else {
-                throw "No token provided";
+                throw `No token provided`;
             }
         } catch(err) {
-            res.json({status: "error", message: err});
+            res.json({status: `error`, message: err});
         }
+    });
+*/
+    // testing auth example
+    app.get(`/api/userss`, authJWT, function (req, res) {
+        User.find({})
+        .then(function(data) {
+            res.json(data);
+        })
+        .catch(function(err) {
+            res.json("err: " + err);
+        });
     });
 
 }

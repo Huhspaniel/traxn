f6dccf706a58ab7f1c9d1cb11c816f2d06d908b5
Huhspaniel <41580234+Huhspaniel@users.noreply.github.com>
2018-12-09 15:48:45 -0500

    Fix csrf token auth and tracklist filter and render

--- client/src/components/Tracklist/TrackList.js
+++ client/src/components/Tracklist/TrackList.js
@@ -1,10 +1,9 @@
 import React from "react";
-import axios from "axios";
 import "../Tracklist/Tracklist.scss";
 
 const Track = props => (
   <div className="track-main">
-    <img className="track-img" src={props.userPic} />
+    <img className="track-img" src={props.userPic} alt={props.userName + ' user picture'} />
     <div className="track-header">
       <p className="track-screen-name">{props.screenName}</p>
       <p className="track-handle">{props.userName}</p>
@@ -25,67 +24,11 @@ const Track = props => (
   </div>
 );
 
-axios
-      .get(`/api/tracks`)
-      .then(res => {
-        console.log(res.data);
-        // this.setState({ feed: tracks });
-      })
-      .catch(err => console.log(err));
-
-class TrackList extends React.Component {
-  state = {
-    feed: []
-  };
-
-  getPublic = () => {
-    axios
-      .get(`/api/tracks`)
-      .then(res => {
-        console.log(res);
-        this.setState({ feed: res.data });
-      })
-      .catch(err => console.log(err));
-  };
-  getFollowing = () => {
-    axios
-      .get(`/api/tracks/following`)
-      .then(res => {
-        console.log(res);
-        this.setState({ feed: res.data });
-      })
-      .catch(err => console.log(err));
-  };
-
-  /*
-    /api/tracks?period={unit},{quantity}&u={userid},{userid}...
-        Gets all tracks, filtering based on time period, and
-        specific users. If {period} is not defined, gets tracks for
-        all time. If {u} is not defined, gets tracks for all users.
-        
-        For example, if {period} = 'week,3', will only get tracks 
-        from past 3 weeks. If {u} = 'user1_id,user2_id,user3_id'
-        will only get tracks posted by user1, user2, user3.
-
-
-    /api/tracks/following?period={unit},{quantity}
-        Only gets tracks posted by those a user is following.
-        Only accessible when user is logged in using JWT authentication.
-        The JWT token will automatically be stored in cookies upon login.
-  */
-
-  render() {
-    if (!this.state.feed[0]) {
-      if (this.props.filter === "public") {
-        this.getPublic();
-      } else if (this.props.filter === "following") {
-        this.getFollowing();
-      }
-    }
-
-    return (
-      <div className="tracklist">
-        {this.state.feed.map(track => (console.log(track) || 
+const TrackList = props => (
+  <div className="tracklist">
+    {props.feed ?
+      props.feed[0] ?
+        props.feed.map(track => (console.log(track) ||
           <Track
             screenName={track.user.displayName}
             userPic="https://www.gstatic.com/webp/gallery/1.jpg"
@@ -94,10 +37,10 @@ class TrackList extends React.Component {
             trackContent={track.content}
             key={track._id}
           />
-        ))}
-      </div>
-    );
-  }
-}
+        ))
+        : <span style={{ color: 'black', padding: '20px' }}>No posts :(</span> // <-- Whatever is here is what will be displayed if there are no posts to show
+      : '' /* <-- Whatever is here is what will be displayed when posts have not loaded yet */}
+  </div>
+)
 
 export default TrackList;

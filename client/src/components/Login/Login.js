701ac1a32d2978f29deeda47ada458c2fdf6c0e1
walkerSwift <walker@empirex.tech>
2018-12-09 02:48:29 -0500

    added sign up inside login.js .. need to fix axios request for sign up and user

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -4,20 +4,40 @@ import axios from 'axios';
 
 const Login = (props) => (
             <div className="login-div">
-                <p onClick={props.toggleHandler}>Login</p>
+                <p onClick={props.toggleHandler}>Login/Sign Up</p>
                 <Modal isOpen={props.isActive}>
-                <input onChange={props.changeHandler} 
-                type="text" name={props.name} 
-                value={props.value} placeholder="username" />
+                <h3>Login</h3>
                 <input onChange={props.changeHandler} 
                 type="text" name="username"
+                value={props.userValue} placeholder="username" />
+                <input onChange={props.changeHandler} 
+                type="text"  name="password"
+                value={props.passValue} placeholder="password" />
+                <button className="send-login" onClick={props.loginHandler}>Login</button>
+
+                <br/>
+                <h4>Or</h4>
+                <br/>
+                
+                <h3>Sign Up</h3>
+                <input onChange={props.changeHandler}
+                type="text" name="username"
+                value={props.value} placeholder="username" />
+                <input onChange={props.changeHandler}
+                type="text" name="password"
                 value={props.value} placeholder="password" />
-                <p className="send-login" onClick={props.loginHandler}>Login</p>
+                <input onChange={props.changeHandler}
+                type="text" name="email"
+                value={props.value} placeholder="email" />
+                <input onChange={props.changeHandler}
+                type="text" name="displayName"
+                value={props.value} placeholder="display name" />
+                <button className="signup-btn" onClick={props.signupHandler}>Sign Up</button>
+                
                 <p className="login-btn" onClick={props.toggleHandler}>Close</p>
                     
                 </Modal>
-            </div>
-                
+            </div>      
 )
 
 
@@ -30,14 +50,28 @@ class LoginModal extends React.Component {
     }
 
     sendLogin = () => {
+        let loginCredentials = [];
+        let username = props.userValue;
+        let password = props.passValue
         axios
-          .post(`/login`)
+          .post(`/login`, loginCredentials)
           .then(user => {
             console.log(user);
             this.setState({ loggedIn: true });
           })
           .catch(err => console.log(err));
       };
+
+    createUser = () => {
+        axios
+      .post(`/api/users`, {
+          username: this.p
+      })
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => console.log(err));
+    }
 
     getToken = () => {
         axios
@@ -71,6 +105,7 @@ class LoginModal extends React.Component {
         return (
             <div className='login-modal'>
                 <Login
+                signupHandler = {this.createUser}
                 loginHandler = {this.sendLogin}
                 isActive = {this.state.isActive}
                 toggleHandler={this.handleToggle}
